aff88177be45f0116f20e4f685786890198859cd
fneim <neimfargin@gmx.com.tr>
2017-08-18 00:49:41 -0700

    separate js and html

---
+++ assets/javascript/game.js
@@ -0,0 +1,43 @@
+	//declared and initialized array alphabet
+	//Created 
+
+		var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+		var wins = 0;
+		var losses = 0;
+		var guesses = 5;
+
+		//randomLetter will chooses a random string from the array
+		var randomLetter = alphabet[Math.floor(Math.random()* alphabet.length)];
+
+		//When user click a letter function event will be called. It will check user input for a lower case string and compare user input to a random string from the array
+		document.onkeyup = function(event) {
+
+		var userInput = event.key;
+		//if user input match random letter, will call function youWin, else will call function youLose
+		if (userInput === randomLetter) {
+			wins++;
+			guesses = 5;
+			
+		} else {
+			losses--;
+
+			guesses--;
+
+			if(guesses === 0) {
+				alert("You have no more guesses.");
+				alert("The computer's guess was " + randomLetter);
+			}
+			}
+		};
+
+		document.write("Wins: " + wins + "<br>");
+		document.write("Losses: " + losses + "<br>");
+		document.write("Guesses Left: " + guesses);
+		
+
+
+	
+	// var guessesMade = [];
+	// guessesMade.push(userInput);
+	// document.getElementById("guessesMade").innerHTML = "Your guesses so far: " + guessesMade;
